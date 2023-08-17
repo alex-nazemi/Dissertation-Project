1b974b0d6659aefacdd5f838551eb82d91f4849d
alex-nazemi <alexandernazemi@gmail.com>
2023-08-17 22:28:59 +0100

    changes to training page component, added stimuli images

--- emotion-recognition/src/Components/TrainingPage.js
+++ emotion-recognition/src/Components/TrainingPage.js
@@ -6,81 +6,96 @@ const TrainingContainer = styled.div`
   text-align: center;
 `;
 
-const Video = styled.video`
-  width: 60%;
-  max-width: 400px;
-  height: auto;
-  display: block;
-  margin: 0 auto;
-`;
-
 const TrainingPage = () => {
-  const [emotion, setEmotion] = useState("");
   const videoRef = useRef(null);
   const [model, setModel] = useState(null);
+  const [stimulusImage, setStimulusImage] = useState("");
+  const [userChoices, setUserChoices] = useState([]);
+  const [feedback, setFeedback] = useState("");
+  const [round, setRound] = useState(0);
+  const MAX_ROUNDS = 1;
+
+  const emotionLabels = [
+    "anger",
+    "disgust",
+    "fear",
+    "happy",
+    "neutral",
+    "sad",
+    "surprise",
+  ];
+  const stimuliPaths = "EmotionalStimuli";
+
+  function getRandomImagePath(emotion) {
+    const randomIndex = Math.floor(Math.random() * 6) + 1;
+    return `/EmotionalStimuli/${emotion}/${emotion}${randomIndex}.jpg`;
+  }
 
   useEffect(() => {
     async function setupWebcam() {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      videoRef.current.srcObject = stream;
-      return new Promise((resolve) => {
-        videoRef.current.onloadedmetadata = () => {
-          resolve(videoRef.current);
-        };
-      });
+      await navigator.mediaDevices.getUserMedia({ video: true });
     }
 
     async function loadModel() {
-      const loadedModel = await tf.loadLayersModel("/model/model.json");
+      const loadedModel = await tf.loadLayersModel("/model_output/model.json");
       setModel(loadedModel);
     }
 
-    setupWebcam().then((video) => {
+    setupWebcam().then(() => {
       loadModel().then(() => {
         if (model) {
-          detectEmotion(video);
+          const randomEmotion =
+            emotionLabels[Math.floor(Math.random() * emotionLabels.length)];
+          setStimulusImage(getRandomImagePath(randomEmotion));
         }
       });
     });
   }, [model]);
 
   const detectEmotion = (video) => {
-    return tf.tidy(() => {
-      const tensor = tf.browser
-        .fromPixels(video)
-        .resizeNearestNeighbor([48, 48])
-        .mean(2)
-        .toFloat()
-        .expandDims(-1)
-        .expandDims(0);
-      const predictions = model.predict(tensor);
-
-      const emotionLabels = [
-        "anger",
-        "contempt",
-        "disgust",
-        "fear",
-        "happy",
-        "neutral",
-        "sad",
-        "surprise",
-      ];
-      const predictedIndex = predictions.argMax(-1).dataSync()[0];
-      const detectedEmotion = emotionLabels[predictedIndex];
-      console.log(`Detected emotion: ${detectedEmotion}`);
-    });
+    // Detect emotion as before
+    const tensor = tf.browser
+      .fromPixels(video)
+      .resizeNearestNeighbor([48, 48])
+      .mean(2)
+      .toFloat()
+      .expandDims(-1)
+      .expandDims(0);
+    const predictions = model.predict(tensor);
+    const predictedIndex = predictions.argMax(-1).dataSync()[0];
+    const detectedEmotion = emotionLabels[predictedIndex];
+    return detectedEmotion;
   };
 
-  const checkEmotion = (userGuess) => {
-    if (userGuess === emotion) {
+  const handleUserChoice = async (chosenEmotion) => {
+    const detectedEmotion = await detectEmotion(videoRef.current);
+    if (chosenEmotion === detectedEmotion) {
+      setFeedback("Your feelings match the model's prediction. Good job!");
     } else {
+      setFeedback(`Model detected: ${detectedEmotion}. Try again.`);
+      // Here, you can add code to show a human face image corresponding to 'detectedEmotion' and ask the user what emotion it is.
+      // For simplicity, I'm skipping that part in this code.
+    }
+
+    // Move to the next round or finish the game
+    if (round < MAX_ROUNDS) {
+      const randomEmotion =
+        emotionLabels[Math.floor(Math.random() * emotionLabels.length)];
+      setStimulusImage(getRandomImagePath(randomEmotion));
+      setRound(round + 1);
+    } else {
+      setFeedback("Thank you for participating!");
+      setStimulusImage(""); // Hide the stimulus image
     }
   };
 
   return (
     <TrainingContainer>
-      <Video ref={videoRef} autoPlay={true}></Video>
-      <div></div>
+      {stimulusImage && <img src={stimulusImage} alt="Stimulus" />}
+      {userChoices.map((choice) => (
+        <button onClick={() => handleUserChoice(choice)}>{choice}</button>
+      ))}
+      {feedback && <p>{feedback}</p>}
     </TrainingContainer>
   );
 };
